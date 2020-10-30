42497a44db5b3a42b04b3103dbfa1f3f3944a979
Walter C <walter.condori.10@gmail.com>
2020-10-29 19:22:29 -0700

    Added a feature for comments

--- Parstagram/AppDelegate.swift
+++ Parstagram/AppDelegate.swift
@@ -11,7 +11,7 @@ import Parse
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
@@ -22,6 +22,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         }
         Parse.initialize(with: parseConfig)
         // Override point for customization after application launch.
+        
         return true
     }
 
